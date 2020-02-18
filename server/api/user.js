e1b14495130e74d0b45cfa36a19180bd87f0d692
chris <christopherleboeuf1@gmail.com>
2020-02-18 15:17:47 -0600

    Account creation and account lookup routs setup

--- server/api/user.js
+++ server/api/user.js
@@ -1,7 +1,29 @@
 const { Router } = require('express');
-const db = require('../db');
+const { findUser, createUser } = require('../db');
 
 const userRouter = Router();
+
+userRouter.post('/:name', (req, res) => {
+  createUser(req.params.name)
+    .then(() => {
+      res.send('Account created!');
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(500);
+    });
+});
+
+userRouter.get('/:name', (req, res) => {
+  findUser(req.params.name)
+    .then((user) => {
+      res.send(user);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(500);
+    });
+});
 
 module.exports = {
   userRouter,
