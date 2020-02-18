f1a4c134f1a6ae82a342b34aa2c72f5cc0453484
Thomas Garza <tbgarza2@gmail.com>
2020-02-18 17:58:48 -0600

    created a component to display the songlist per each playlist

---
+++ client/src/components/PlaylistSongs.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+class PlaylistSongs extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h4>Songs</h4>
+      </div>
+    );
+  }
+}
+
+export default PlaylistSongs;
