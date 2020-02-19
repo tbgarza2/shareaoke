69d35ee8149950a013fd3f81c04afb387e66f83d
Thomas Garza <tbgarza2@gmail.com>
2020-02-18 23:39:52 -0600

    Added display toggle for friends on friendslist

--- client/src/components/FriendsList.jsx
+++ client/src/components/FriendsList.jsx
@@ -6,15 +6,24 @@ class FriendsList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      display: false,
     };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(state => ({
+      display: !state.display,
+    }))
   }
 
   render() {
     const { friends } = this.props;
+    const { display } = this.state;
     return (
       <div>
-        <h3>Friends</h3>
-        {friends.map(friend => <Friend friend={friend} />)}
+        <h3 onClick={this.handleClick}>Friends</h3>
+        {display ? friends.map(friend => <Friend friend={friend} key={friend} />) : null}
       </div>
     );
   }
