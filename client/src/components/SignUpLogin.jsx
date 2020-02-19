985209856f25298a52e2bd3ed708fb0885a31fc7
Thomas Garza <tbgarza2@gmail.com>
2020-02-18 18:02:08 -0600

    created a signup component to render on App.jsx - functionality still needs to be created

---
+++ client/src/components/SignUpLogin.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class SignUpLogin extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+
+  render() {
+    return (
+      <div>
+        <h1>shareaoke</h1>
+      </div>
+    );
+  }
+}
+
+export default SignUpLogin;
