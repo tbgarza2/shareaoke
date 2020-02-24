4ac9d7602ae3275a115ed1e24404d55fb089c22f
William Steck <steckwilliam@gmail.com>
2020-02-24 12:36:37 -0600

    Changed routing

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const parser = require('body-parser');
 const path = require('path');
 const { apiRouter } = require('./api/index');
+const { spotifyRouter } = require('./auth-server/authorization_code/app');
 
 const app = express();
 
@@ -11,7 +12,10 @@ const CLIENT_PATH = path.join(__dirname, '../client/dist');
 app.use(express.static(CLIENT_PATH));
 app.use(parser.json());
 app.use('/api', apiRouter);
+app.use('/spotify', spotifyRouter);
 
 app.listen(PORT, () => {
   console.log(`Listening on :${PORT} 🚀`);
 });
+
+module.exports.app = app;
