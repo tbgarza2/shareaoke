0181ac8f051c4c09e2212c304a92f389d67b6661
chris <christopherleboeuf1@gmail.com>
2020-02-25 15:52:31 -0600

    Added routes for playlists

--- server/api/index.js
+++ server/api/index.js
@@ -1,10 +1,12 @@
 const { Router } = require('express');
 const { userRouter } = require('./user');
 const { songRouter } = require('./song');
+const { playlistRouter } = require('./playlist');
 
 const apiRouter = Router();
 
 apiRouter.use('/user', userRouter);
 apiRouter.use('/song', songRouter);
+apiRouter.use('/playlist', playlistRouter);
 
 module.exports.apiRouter = apiRouter;
