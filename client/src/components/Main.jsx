faf5ed0a940d1cb8e96078bc2afde641061cce7a
William Steck <steckwilliam@gmail.com>
2020-02-26 14:53:46 -0600

    Made token dynamic, added fetch request to get users spotify ID

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,18 +1,41 @@
 import React from 'react';
+import querystring from 'querystring';
 import { Link } from 'react-router-dom';
 
 class Main extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      token: '',
+      spotifyId: '',
     };
   }
 
+  componentDidMount() {
+    const parsedToken = querystring.parse(window.location.hash);
+    const token = parsedToken['#/main/#access_token'];
+
+    this.setState({
+      token,
+    });
+
+    fetch('https://api.spotify.com/v1/me', {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${token}`
+      },
+    })
+      .then(response => response.json())
+      .then(data => this.setState({
+        spotifyId: data.display_name,
+      }));
+  }
+
   render() {
-    const { user, friends, playlists } = this.props;
+    const { spotifyId } = this.state;
     return (
       <div>
-        <h1>{user}</h1>
+        <h1>{spotifyId}</h1>
         <ul>
           <li>
             <Link to="/createplaylist">Create a playlist</Link>
