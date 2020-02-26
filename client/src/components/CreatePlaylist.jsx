1344a710464f41e0ac36b6c127ad9b9f8377d2d2
William Steck <steckwilliam@gmail.com>
2020-02-25 19:13:34 -0600

    Song clicked on successfully added to list of songs to add to playlist

--- client/src/components/CreatePlaylist.jsx
+++ client/src/components/CreatePlaylist.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import SpotifyResults from './SpotifyResults.jsx';
 
 class CreatePlaylist extends React.Component {
   constructor(props) {
@@ -6,11 +7,14 @@ class CreatePlaylist extends React.Component {
     this.state = {
       playlistName: '',
       song: '',
+      songs: [],
       songData: [],
+      display: false,
     };
     this.handlePlaylistNameChange = this.handlePlaylistNameChange.bind(this);
     this.handleSongNameChange = this.handleSongNameChange.bind(this);
     this.searchSpotifyForSong = this.searchSpotifyForSong.bind(this);
+    this.addSongToPlaylist = this.addSongToPlaylist.bind(this);
   }
 
   handlePlaylistNameChange(e) {
@@ -26,7 +30,7 @@ class CreatePlaylist extends React.Component {
   }
 
   searchSpotifyForSong() {
-    const { song } = this.state;
+    const { song, display } = this.state;
     let songQuery = song.split(' ').join('%20');
 
     const token = 'BQBb55OmRMZlIug4TakVfhX8k_rzOxywzELz8tyVs20HFoPhRCDMOUkzYa4q4GylTtO4lMicqrS3_Am-rm2-IhsLNOuOEEVHJDSgQ9ZsXwv-JGaX7bC2mX4dKAG8eInZHlkTJIVtYq_gpU7D-eiW2TEdCOL5LqA';
@@ -40,14 +44,27 @@ class CreatePlaylist extends React.Component {
       },
     })
       .then(response => response.json())
-      .then(data => console.log(data.items));
+      .then(data => this.setState({
+        songData: data.tracks.items,
+        display: !display,
+      }));
+  }
+
+  addSongToPlaylist(song) {
+    const { songs } = this.state;
+
+    this.setState({
+      songs: songs.concat(song),
+    });
+
   }
 
   createPlaylist() {
   }
 
   render() {
-    const { playlistName, song } = this.state;
+    const { playlistName, song, display, songData } = this.state;
+
     return (
       <div>
         <h1>Create a new playlist</h1>
@@ -58,6 +75,9 @@ class CreatePlaylist extends React.Component {
         <div>
           Search for a song: <input value={song} onChange={this.handleSongNameChange} />
           <button onClick={this.searchSpotifyForSong} type="button">Search for song</button>
+        </div>
+        <div>
+          {display ? <SpotifyResults songData={songData} addSong={this.addSongToPlaylist} /> : null}
         </div>
         <button onClick={this.createPlaylist} type="button">Make Playlist</button>
       </div>
