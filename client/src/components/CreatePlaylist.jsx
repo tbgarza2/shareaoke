909e917930e6a594c43f50639a5a6c1431bd5e5c
William Steck <steckwilliam@gmail.com>
2020-02-25 18:05:18 -0600

    search and fethc are working. Recieving the first ten results when searching for a song.

--- client/src/components/CreatePlaylist.jsx
+++ client/src/components/CreatePlaylist.jsx
@@ -5,25 +5,60 @@ class CreatePlaylist extends React.Component {
     super(props);
     this.state = {
       playlistName: '',
-      songs: [],
-      friends: [],
+      song: '',
     };
     this.handlePlaylistNameChange = this.handlePlaylistNameChange.bind(this);
+    this.handleSongNameChange = this.handleSongNameChange.bind(this);
+    this.searchSpotifyForSong = this.searchSpotifyForSong.bind(this);
   }
 
   handlePlaylistNameChange(e) {
-    this.setState = ({
+    this.setState({
       playlistName: e.target.value,
     });
   }
 
+  handleSongNameChange(e) {
+    this.setState({
+      song: e.target.value,
+    });
+  }
+
+  searchSpotifyForSong() {
+    const { song } = this.state;
+    let songQuery = song.split(' ').join('%20');
+
+    const token = 'BQBb55OmRMZlIug4TakVfhX8k_rzOxywzELz8tyVs20HFoPhRCDMOUkzYa4q4GylTtO4lMicqrS3_Am-rm2-IhsLNOuOEEVHJDSgQ9ZsXwv-JGaX7bC2mX4dKAG8eInZHlkTJIVtYq_gpU7D-eiW2TEdCOL5LqA';
+    // Need to update later to make dyanmic and pull from App.jsx
+
+    fetch(`https://api.spotify.com/v1/search?q=${songQuery}&type=track&market=US&limit=10`, {
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`,
+      },
+    })
+      .then(response => response.json())
+      .then(data => console.log(data));
+  }
+
+  createPlaylist() {
+  }
+
   render() {
-    const { playlistName, songs, friends } = this.state;
+    const { playlistName, song } = this.state;
     return (
       <div>
-        Playlist Name<input value={playlistName} onChange={this.handlePlaylistNameChange} />
-        Add Songs<input />
-        Add Friends<input />
+        <h1>Create a new playlist</h1>
+        <p>Start making a new playlist for your next Shareaoke party</p>
+        <div>
+          Playlist Name: <input value={playlistName} onChange={this.handlePlaylistNameChange} />
+        </div>
+        <div>
+          Search for a song: <input value={song} onChange={this.handleSongNameChange} />
+          <button onClick={this.searchSpotifyForSong} type="button">Search for song</button>
+        </div>
+        <button onClick={this.createPlaylist} type="button">Make Playlist</button>
       </div>
     );
   }
