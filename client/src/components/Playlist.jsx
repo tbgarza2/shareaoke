35db5a9887961dd08746beff7b3129e570b2922b
William Steck <steckwilliam@gmail.com>
2020-02-26 19:09:08 -0600

    Added functionality for searching for songs and choosing which song you want to add to your playlist

--- client/src/components/Playlist.jsx
+++ client/src/components/Playlist.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import Songs from './Songs.jsx';
+import SpotifyResults from './SpotifyResults.jsx';
 
 class Playlist extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       currentPlaylist: '',
+      song: '',
+      songs: [],
+      songData: [],
+      display: false,
     };
+
+    this.handleSongNameChange = this.handleSongNameChange.bind(this);
+    this.searchSpotifyForSong = this.searchSpotifyForSong.bind(this);
+    this.addSongToPlaylist = this.addSongToPlaylist.bind(this);
   }
 
   componentDidMount() {
@@ -23,8 +32,42 @@ class Playlist extends React.Component {
     }
   }
 
+  handleSongNameChange(e) {
+    this.setState({
+      song: e.target.value,
+    });
+  }
+
+  searchSpotifyForSong() {
+    const { song, display } = this.state;
+    let songQuery = song.split(' ').join('%20');
+
+    const { token } = this.props.location.state;
+
+    fetch(`https://api.spotify.com/v1/search?q=${songQuery}&type=track&market=US&limit=10`, {
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`,
+      },
+    })
+      .then(response => response.json())
+      .then(data => this.setState({
+        songData: data.tracks.items,
+        display: !display,
+      }));
+  }
+
+  addSongToPlaylist(song) {
+    const { songs } = this.state;
+
+    this.setState({
+      songs: songs.concat(song),
+    });
+  }
+
   render() {
-    const { currentPlaylist } = this.state;
+    const { currentPlaylist, song, songData, display } = this.state;
     const { description } = this.props.location.state;
 
     return (
@@ -32,6 +75,13 @@ class Playlist extends React.Component {
         <h3>{currentPlaylist}</h3>
         <p>{description}</p>
         <Songs />
+        <div>
+          Search for a song: <input value={song} onChange={this.handleSongNameChange} />
+          <button onClick={this.searchSpotifyForSong} type="button">Search for song</button>
+        </div>
+        <div>
+          {display ? <SpotifyResults songData={songData} addSong={this.addSongToPlaylist} /> : null}
+        </div>
       </div>
     );
   }
