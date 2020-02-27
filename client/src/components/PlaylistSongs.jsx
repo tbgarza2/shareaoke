581f98eb10c2b45358203f43e9a6b9197ab74a7f
William Steck <steckwilliam@gmail.com>
2020-02-26 19:28:44 -0600

    Removed old code

--- client/src/components/PlaylistSongs.jsx
+++ client/src/components/PlaylistSongs.jsx
@@ -1,35 +1,3 @@
-// import React from 'react';
-// import Songs from './Songs.jsx';
-
-// class PlaylistSongs extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       display: false,
-//     };
-//     this.handleClick = this.handleClick.bind(this);
-//   }
-
-//   handleClick() {
-//     this.setState(state => ({
-//       display: !state.display,
-//     }));
-//   }
-
-//   render() {
-//     const { playlist } = this.props;
-//     const { display } = this.state;
-//     return (
-//       <div>
-//         <h4 key={playlist} onClick={this.handleClick}>{playlist}</h4>
-//         { display ? <Songs /> : null}
-//       </div>
-//     );
-//   }
-// }
-
-// export default PlaylistSongs;
-
 import React from 'react';
 import { Link } from 'react-router-dom';
 
