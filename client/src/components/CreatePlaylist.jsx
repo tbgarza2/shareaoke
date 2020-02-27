9afbd0df4a8211693eb7af1310c174b5ac00ae0f
William Steck <steckwilliam@gmail.com>
2020-02-26 23:07:24 -0600

    Commit for merge

--- client/src/components/CreatePlaylist.jsx
+++ client/src/components/CreatePlaylist.jsx
@@ -1,51 +1,88 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 class CreatePlaylist extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      playlistName: '',
-      description: '',
+      id_user: 0,
+      userName: 'nas',
+      playlistName: 'Astro World',
+      description: 'travis scott mix',
     };
     this.handlePlaylistNameChange = this.handlePlaylistNameChange.bind(this);
     this.handleDescriptionChange = this.handleDescriptionChange.bind(this);
+    this.addPlaylist = this.addPlaylist.bind(this);
+    this.createUser = this.createUser.bind(this);
+    this.getUserInfo = this.getUserInfo.bind(this);
+  }
+
+  getUserInfo(userName) {
+    console.log('fetching user info');
+    return axios
+      .get(`/api/user/${userName}`)
+      .then(userInfo => {
+        console.log(userInfo.data[0].id);
+        this.setState({
+          id_user: userInfo.data[0].id,
+        });
+        this.addPlaylist();
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  createUser() {
+    console.log('creating user');
+    const { userName } = this.state;
+    return axios
+      .post(`/api/user/${userName}`)
+      .then(userInfo => {
+        console.log(userInfo.data);
+        this.getUserInfo(userName);
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 
   handlePlaylistNameChange(e) {
-    this.setState({
+    this.setState = ({
       playlistName: e.target.value,
     });
   }
 
   handleDescriptionChange(e) {
-    this.setState({
-      description: e.target.value,
+    this.setState = ({
+      playlistName: e.target.value,
     });
+  }
+
+
+  addPlaylist() {
+    console.log('click');
+    const { id_user, playlistName, description } = this.state;
+    console.log(id_user);
+    console.log(playlistName);
+    console.log(description);
+    return axios
+      .post('/api/playlist', { id_user, playlistName, description })
+      .then(response => {
+        console.log('added to database', response);
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 
   render() {
     const { playlistName, description } = this.state;
-    const { spotifyId, token } = this.props.location.state;
-
     return (
       <div>
-        <h1>Create a new playlist</h1>
-        <p>Start making a new playlist for your next Shareaoke party</p>
-        <div>
-          Playlist Name: <input value={playlistName} onChange={this.handlePlaylistNameChange} />
-        </div>
-        <div>
-          Enter a description:
-          <textarea
-            style={{ width: 300 }} 
-            value={description} 
-            onChange={this.handleDescriptionChange}
-          />
-        </div>
-        <Link to={{ pathname: '/playlist', state: { playlistName, description, spotifyId, token } }}>
-          <button onClick={this.createPlaylist} type="button">Create Playlist</button>
-        </Link>
+        Playlist Name<input value={playlistName} onChange={this.handlePlaylistNameChange} />
+        Add Description<input value={description} onChange={this.handleDescriptionChange} />
+        <button onClick={this.createUser}>Save</button>
       </div>
     );
   }
