fbb6d336fff77ac569312fad3f5336fc58ecf526
William Steck <steckwilliam@gmail.com>
2020-02-26 18:23:33 -0600

    Playlist now redners title and decription with conditonal depending one where it was linked from

--- client/src/components/Playlist.jsx
+++ client/src/components/Playlist.jsx
@@ -5,14 +5,32 @@ class Playlist extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      currentPlaylist: '',
     };
   }
 
+  componentDidMount() {
+    if (this.props.location.state.playlist) {
+      this.setState({
+        currentPlaylist: this.props.location.state.playlist,
+      });
+    }
+
+    if (this.props.location.state.playlistName) {
+      this.setState({
+        currentPlaylist: this.props.location.state.playlistName,
+      });
+    }
+  }
+
   render() {
-    const { playlist } = this.props.location.state;
+    const { currentPlaylist } = this.state;
+    const { description } = this.props.location.state;
+
     return (
       <div>
-        <h3>{playlist}</h3>
+        <h3>{currentPlaylist}</h3>
+        <p>{description}</p>
         <Songs />
       </div>
     );
