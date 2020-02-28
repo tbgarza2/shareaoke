980ae219af51b56a07e230cd1acf66e62dd0682f
William Steck <steckwilliam@gmail.com>
2020-02-27 19:46:23 -0600

    Added route for searching for songs

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -6,7 +6,7 @@ import FriendsList from './FriendsList.jsx';
 import Playlists from './Playlists.jsx';
 import CreatePlaylist from './CreatePlaylist.jsx';
 import Playlist from './Playlist.jsx';
-import Player from './Player.jsx';
+import SearchForSongs from './SearchForSongs.jsx';
 
 
 class App extends React.Component {
@@ -34,6 +34,7 @@ class App extends React.Component {
           <Route exact path="/friends" render={(routerProps) => (<FriendsList {...routerProps} user={user} friends={friends} />)} />
           <Route exact path="/createplaylist" component={CreatePlaylist} />
           <Route exact path="/playlist" component={Playlist} />
+          <Route exact path="/search" component={SearchForSongs} />
         </Switch>
       </HashRouter>
     );
