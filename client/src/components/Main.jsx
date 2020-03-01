2d54a6f3ddf5baf352f998c3535a5b95fc2eaf13
chris <christopherleboeuf1@gmail.com>
2020-03-01 13:03:08 -0600

    Removed state from main

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -2,22 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 class Main extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      id: 0,
-    };
-  }
-
   render() {
     const { username, token, id_user } = this.props;
-    const { id } = this.state;
     return (
       <div>
         <h1>{username}</h1>
         <ul>
           <li>
-            <Link to={{ pathname: '/createplaylist', state: { id, username, token, id_user } }}>Create a playlist</Link>
+            <Link to={{ pathname: '/createplaylist', state: { username, token, id_user } }}>Create a playlist</Link>
           </li>
           <li>
             <Link to={{ pathname: '/playlists', state: { username, id_user } }}>Playlists</Link>
