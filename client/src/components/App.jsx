bc1b19293348e0d4ef6a3d38dbbb77ae515b4df9
chris <christopherleboeuf1@gmail.com>
2020-03-01 01:53:48 -0600

    Changed the way props are passed around the entire app

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { HashRouter, Switch, Route } from 'react-router-dom';
+import axios from 'axios';
 import Main from './Main.jsx';
 import SignUpLogin from './SignUpLogin.jsx';
-import FriendsList from './FriendsList.jsx';
+import Friends from './Friends.jsx';
 import Playlists from './Playlists.jsx';
 import CreatePlaylist from './CreatePlaylist.jsx';
 import Playlist from './Playlist.jsx';
@@ -13,28 +14,75 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      user: 'tdog',
-      friends: ['bubba', 'bull', 'will', 'chris', 'nas'],
-      playlists: ['Awesome Beats', 'Classical', 'Birthday Party', 'Royal Wedding'],
+      id_user: 0,
+      token: '',
+      username: '',
     };
   }
 
   componentDidMount() {
-    console.log('Mounted!');
+    axios.get('/spotify/user')
+      .then(({ data }) => data.display_name)
+      .then((username) => {
+        this.setState({
+          username,
+        });
+      })
+      .then(() => {
+        this.getUserInfo();
+      })
+      .catch((err) => console.error(err));
+
+    axios.get('/spotify/token')
+      .then(({ data }) => data.access_token)
+      .then((token) => {
+        this.setState({
+          token,
+        });
+      })
+      .catch((err) => console.error(err));
+  }
+
+  getUserInfo() {
+    const { username } = this.state;
+    console.log('Fetching user info');
+    axios.get(`/api/user/${username}`)
+      .then(({ data }) => {
+        this.setState({
+          id_user: data[0].id,
+        });
+      })
+      .catch(() => {
+        this.createUser();
+      });
+  }
+
+  createUser() {
+    console.log('creating user');
+    const { username } = this.state;
+    axios.post(`/api/user/${username}`)
+      .then(({ data }) => {
+        this.setState({
+          id_user: data,
+        });
+      })
+      .catch(err => {
+        console.error(err);
+      });
   }
 
   render() {
-    const { user, friends, playlists } = this.state;
+    const { username, id_user, token } = this.state;
     return (
       <HashRouter>
         <Switch>
           <Route exact path="/" component={SignUpLogin} />
-          <Route exact path="/main" render={(routerProps) => (<Main {...routerProps} user={user} />)} />
-          <Route exact path="/playlists" render={(routerProps) => (<Playlists {...routerProps} user={user} playlists={playlists} />)} />
-          <Route exact path="/friends" render={(routerProps) => (<FriendsList {...routerProps} user={user} friends={friends} />)} />
-          <Route exact path="/createplaylist" component={CreatePlaylist} />
-          <Route exact path="/playlist" component={Playlist} />
-          <Route exact path="/search" component={SearchForSongs} />
+          <Route exact path="/main" render={(routerProps) => (<Main {...routerProps} username={username} id_user={id_user} token={token} />)} />
+          <Route exact path="/playlists" render={(routerProps) => (<Playlists {...routerProps} username={username} id_user={id_user} token={token} />)} />
+          <Route exact path="/friends" render={(routerProps) => (<Friends {...routerProps} username={username} id_user={id_user} token={token} />)} />
+          <Route exact path="/createplaylist" render={(routerProps) => (<CreatePlaylist {...routerProps} username={username} id_user={id_user} token={token} />)} />
+          <Route exact path="/playlist" render={(routerProps) => (<Playlist {...routerProps} username={username} id_user={id_user} token={token} />)} />
+          <Route exact path="/search" render={(routerProps) => (<SearchForSongs {...routerProps} username={username} id_user={id_user} token={token} />)} />
         </Switch>
       </HashRouter>
     );
