2bf21220194ea32dfb39890c1eb68e14c85b36af
William Steck <steckwilliam@gmail.com>
2020-03-01 14:42:55 -0600

    Added styling for SignUpLogin

--- client/src/components/SignUpLogin.jsx
+++ client/src/components/SignUpLogin.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Jumbotron from 'react-bootstrap/Jumbotron';
 
 class SignUpLogin extends React.Component {
   constructor(props) {
@@ -7,15 +9,17 @@ class SignUpLogin extends React.Component {
     };
   }
 
-
   render() {
     return (
-      <div>
-        <h1>Welcome to Shareaoke! Please login with Spotify to continue.</h1>
+      <Jumbotron style={{ textAlign: 'center', background: 'orange' }}>
+        <h1 style={{ color: 'white' }}>Welcome to Shareaoke!</h1>
+        <p style={{ color: 'white' }}>
+          Please login with Spotify to begin creating your very own Shareaoke playlist!
+        </p>
         <a href="http://localhost:8080/spotify/login">
-          <button>Login with Spotify</button>
+          <Button variant="success">Login with Spotify</Button>
         </a>
-      </div>
+      </Jumbotron>
     );
   }
 }
