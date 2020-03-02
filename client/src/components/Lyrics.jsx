0460e118da51d68a18e48a52c503028b6c2a7f57
NaseerHines <Hinesnaseer@gmail.com>
2020-03-01 19:34:03 -0600

    Fixed Lyrics.jsx to make a request to server to grab lyrics and render on page

--- client/src/components/Lyrics.jsx
+++ client/src/components/Lyrics.jsx
@@ -8,32 +8,39 @@ class Lyrics extends React.Component {
       query: `${this.props.queryData.title} ${this.props.queryData.artist}`,
       lyrics: 'Show Lyircs',
     };
-    this.SongData = this.SongData.bind(this);
+    // this.SongData = this.SongData.bind(this);
     this.Search = this.Search.bind(this);
   }
 
   Search() {
     const { query } = this.state;
     console.log(query);
-    axios.get(`/getmusic/${query}`, (data) => {
-      // eslint-disable-next-line no-alert
-      alert('Data Received');
-      console.log(data);
-      if (data.length < 3) {
+    // this.setState({ lyrics: 'changed' });
+    axios
+      .get('/api/lyrics', {
+        params: {
+          query,
+        },
+      })
+      .then(response => {
+        // eslint-disable-next-line no-alert
+        alert('Lyrics Received');
+        console.log(response);
+        this.setState({
+          lyrics: response.data,
+        });
+      })
+      .catch(error => {
         // eslint-disable-next-line no-alert
         alert('No Lyrics Found');
-      } else {
-        this.setState({
-          lyrics: data,
-        });
-      }
-    });
+        console.log(error);
+      });
   }
 
   render() {
     const { lyrics } = this.state;
     return (
-      <div onClick={this.SongData}>
+      <div onClick={this.Search}>
         {lyrics}
       </div>
     );
